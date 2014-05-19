5c0460bde45398ff9439810c4f6e0e3862bdf85b
Pan Thomakos <pan.thomakos@gmail.com>
2014-05-19 15:05:47 -0700

    Update firewall recipe to use iptables-restore.

--- .puppet/dist/firewall/manifests/init.pp
+++ .puppet/dist/firewall/manifests/init.pp
@@ -1,10 +1,14 @@
 class firewall {
+  $restore = '/usr/bin/iptables-restore < /etc/iptables/iptables.rules'
+
   file {
     '/etc/iptables/iptables.rules':
       source => 'puppet:///modules/firewall/iptables.rules',
       ensure => file,
-      notify => Service['iptables'],
+      notify => Exec[$restore],
   }
+
+  exec { $restore: refreshonly => true, }
 
   service {
     'iptables':
