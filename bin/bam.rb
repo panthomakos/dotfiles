577a20adf4b20f8e66b3583d84895be64f59b8a0
Pan Thomakos <pan.thomakos@gmail.com>
2015-04-16 11:20:59 -0700

    Update rbenv, ruby-build and bam.

--- bin/bam.rb
+++ bin/bam.rb
@@ -6,5 +6,11 @@
 #     $ ruby bin/bam.rb c/p
 #     /home/pan/Projects/client/project
 
-glob = ARGV[0].gsub(/\//, '*/')+'*'
-puts Dir.glob("#{ENV['HOME']}/Projects/#{glob}").first
+PROJECT_DIR = "#{ENV['HOME']}/Projects"
+
+if ARGV[0].nil?
+  puts(PROJECT_DIR)
+else
+  glob = ARGV[0].gsub(/\//, '*/')+'*'
+  puts(Dir.glob("#{PROJECT_DIR}/#{glob}").first)
+end
