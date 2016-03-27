856c91f3394f9d202dd77a7beb03f3a63d35780b
Pan Thomakos <pan.thomakos@gmail.com>
2016-03-26 20:41:48 -0700

    Fix xmonad/xmobar configuration
    Added the `handleEventHook` section to resolve an issue where chromium
    was not respecting xmobar on the first desktop screen.
    
    Reformatting is just for cleanup and un-related to the fix.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,35 +1,35 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import qualified XMonad.StackSet as W
-import XMonad.Actions.CycleWS
-import qualified Data.Map as M
-import System.Exit
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig
-import System.IO
-import Graphics.X11.ExtraTypes.XF86
+import qualified Data.Map                     as M
+import           Graphics.X11.ExtraTypes.XF86
+import           System.Exit
+import           System.IO
+import           XMonad
+import           XMonad.Actions.CycleWS
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import qualified XMonad.StackSet              as W
+import           XMonad.Util.EZConfig
+import           XMonad.Util.Run              (spawnPipe)
 
-import XMonad.Prompt
-import XMonad.Prompt.Shell
+import           XMonad.Prompt
+import           XMonad.Prompt.Shell
 
-import XMonad.Hooks.UrgencyHook
+import           XMonad.Hooks.UrgencyHook
 
 myKeys c = mkKeymap c $
-	[ ("M-<Return>", spawn $ XMonad.terminal c)
-	, ("<XF86AudioRaiseVolume>", spawn "amixer -D pulse set Master 5%+ unmute")
-	, ("<XF86AudioLowerVolume>", spawn "amixer -D pulse set Master 5%- unmute")
-	, ("<XF86AudioMute>", spawn "amixer -D pulse set Master toggle")
-  , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 20")
-  , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 20")
-	, ("M-g", spawn "chromium")
-	, ("M-q", spawn "i3lock")
-	, ("M-p", shellPrompt defaultXPConfig)
-  , ("M-m", windows W.focusMaster)
-  , ("M-s", windows W.swapMaster)
-  , ("M-u", focusUrgent)
-  , ("M-S-s", withFocused $ windows . W.sink)
-	]
+    [ ("M-<Return>", spawn $ XMonad.terminal c)
+    , ("<XF86AudioRaiseVolume>", spawn "amixer -D pulse set Master 5%+ unmute")
+    , ("<XF86AudioLowerVolume>", spawn "amixer -D pulse set Master 5%- unmute")
+    , ("<XF86AudioMute>", spawn "amixer -D pulse set Master toggle")
+    , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 20")
+    , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 20")
+    , ("M-g", spawn "chrome")
+    , ("M-q", spawn "i3lock")
+    , ("M-p", shellPrompt defaultXPConfig)
+    , ("M-m", windows W.focusMaster)
+    , ("M-s", windows W.swapMaster)
+    , ("M-u", focusUrgent)
+    , ("M-S-s", withFocused $ windows . W.sink)
+    ]
 
 myManageHook = composeAll
   [ title =? "Authy" --> doFloat
@@ -53,6 +53,9 @@ main = do
     , keys = \c -> myKeys c `M.union` keys defaultConfig c
     , manageHook = myManageHook <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  myLayouts
+    , handleEventHook = mconcat
+                      [ docksEventHook
+                      , handleEventHook defaultConfig ]
     , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn xmproc }
     , terminal = "urxvt -e tmux"
     , normalBorderColor = "#000000"
