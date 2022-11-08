f17642bafb8ecfc174766b093999cf76fd66dd2e
Pan Thomakos <pan.thomakos@gmail.com>
2022-11-07 23:11:16 -0800

    Keyboard INO Cleanup and Keymap Adjustments

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -68,98 +68,40 @@
 
 enum { MACRO_VERSION_INFO };
 
-
-
-/** The Model 01's key layouts are defined as 'keymaps'. By default, there are three
-  * keymaps: The standard QWERTY keymap, the "Function layer" keymap and the "Numpad"
-  * keymap.
-  *
-  * Each keymap is defined as a list using the 'KEYMAP_STACKED' macro, built
-  * of first the left hand's layout, followed by the right hand's layout.
-  *
-  * Keymaps typically consist mostly of `Key_` definitions. There are many, many keys
-  * defined as part of the USB HID Keyboard specification. You can find the names
-  * (if not yet the explanations) for all the standard `Key_` defintions offered by
-  * Kaleidoscope in these files:
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_keyboard.h
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_consumerctl.h
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_sysctl.h
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_keymaps.h
-  *
-  * Additional things that should be documented here include
-  *   using ___ to let keypresses fall through to the previously active layer
-  *   using XXX to mark a keyswitch as 'blocked' on this layer
-  *   using ShiftToLayer() and LockLayer() keys to change the active keymap.
-  *   keeping NUM and FN consistent and accessible on all layers
-  *
-  * The PROG key is special, since it is how you indicate to the board that you
-  * want to flash the firmware. However, it can be remapped to a regular key.
-  * When the keyboard boots, it first looks to see whether the PROG key is held
-  * down; if it is, it simply awaits further flashing instructions. If it is
-  * not, it continues loading the rest of the firmware and the keyboard
-  * functions normally, with whatever binding you have set to PROG. More detail
-  * here: https://community.keyboard.io/t/how-the-prog-key-gets-you-into-the-bootloader/506/8
-  *
-  * The "keymaps" data structure is a list of the keymaps compiled into the firmware.
-  * The order of keymaps in the list is important, as the ShiftToLayer(#) and LockLayer(#)
-  * macros switch to key layers based on this list.
-  *
-  *
-
-  * A key defined as 'ShiftToLayer(FUNCTION)' will switch to FUNCTION while held.
-  * Similarly, a key defined as 'LockLayer(NUMPAD)' will switch to NUMPAD when tapped.
-  */
-
-/**
-  * Layers are "0-indexed" -- That is the first one is layer 0. The second one is layer 1.
-  * The third one is layer 2.
-  * This 'enum' lets us use names like QWERTY, FUNCTION, and NUMPAD in place of
-  * the numbers 0, 1 and 2.
-  *
-  */
-
 enum { PRIMARY, FUNCTION }; // layers
-
-/* This comment temporarily turns off astyle's indent enforcement
- *   so we can make the keymaps actually resemble the physical key layout better
- */
-// *INDENT-OFF*
 
 KEYMAPS(
 
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Spacebar,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+  (___,             Key_1,          Key_2,        Key_3,            Key_4,      Key_5,                    Key_LEDEffectNext,
+   Key_Backtick,    Key_Q,          Key_W,        Key_E,            Key_R,      Key_T,                    Key_Tab,
+   Key_PageUp,      Key_A,          Key_S,        Key_D,            Key_F,      Key_G,
+   Key_PageDown,    Key_Z,          Key_X,        Key_C,            Key_V,      Key_B,                    Key_Spacebar,
+   Key_LeftControl, Key_Backspace,  Key_LeftGui,  Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   ___,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___,
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   ___,   Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
+   ___,             Key_6,          Key_7,        Key_8,            Key_9,      Key_0,                    ___,
+   Key_Enter,       Key_Y,          Key_U,        Key_I,            Key_O,      Key_P,                    Key_Equals,
+                    Key_H,          Key_J,        Key_K,            Key_L,      Key_Semicolon,            Key_Quote,
+   ___,             Key_N,          Key_M,        Key_Comma,        Key_Period, Key_Slash,                Key_Minus,
+   Key_RightShift,  Key_RightAlt,   Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,          Key_F4,           Key_F5,                   Key_CapsLock,
-   Key_Tab,  ___,              Key_mouseUp, Key_LeftBracket, Key_RightBracket, Consumer_VolumeIncrement, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR,      Key_mouseBtnL,    Consumer_VolumeDecrement,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,             Key_mouseBtnM,    Consumer_Mute,            Key_LeftAlt,
-   ___, Key_Delete, ___, ___,
+  (___,             Key_F1,         Key_F2,       Key_F3,           Key_F4,     Key_F5,                   Key_CapsLock,
+   Key_Tab,         ___,            Key_7,        Key_8,            Key_9,      Consumer_VolumeIncrement, Key_mouseWarpNE,
+   Key_Home,        Key_mouseL,     Key_4,        Key_5,            Key_6,      Consumer_VolumeDecrement,
+   Key_End,         Key_0,          Key_1,        Key_2,            Key_3,      Consumer_Mute,            Key_LeftAlt,
+   ___,             Key_Delete,     ___,          ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          LSHIFT(Key_Minus),      Key_LeftParen,            Key_RightParen,           ___,             Key_Backslash,    Key_Pipe,
+   Consumer_ScanPreviousTrack, Key_F6,            Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   Consumer_PlaySlashPause,    LSHIFT(Key_1),     Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+                               Key_LeftArrow,     Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
+   Key_PcApplication,          LSHIFT(Key_Minus), Key_LeftParen,            Key_RightParen,           ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
    ___)
-) // KEYMAPS(
-
-/* Re-enable astyle's indent enforcement */
-// *INDENT-ON*
+)
 
 /** versionInfoMacro handles the 'firmware version info' macro
  *  When a key bound to the macro is pressed, this macro
@@ -333,7 +275,7 @@ void setup() {
     /* , {Key_LeftAlt, LSHIFT(Key_Minus), 250} // underscore */
     , SPACECADET_MAP_END
   };
-  SpaceCadet.map = spacecadetmap;
+  SpaceCadet.setMap(spacecadetmap);
 
   // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
   // This draws more than 500mA, but looks much nicer than a dimmer effect
